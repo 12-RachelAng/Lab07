8f85a58b601d6493941d7c40d32e9c6511de4279
12-RachelAng <cherryzx04@gmail.com>
2021-10-19 16:08:01 +0800

    Player (AngryDuck) able to fly
    When Spacebar is pressed, duck will fly slightly upward

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,8 +6,14 @@ public class Player : MonoBehaviour
 {
     private Animation thisAnimation;
 
+    public Vector3 Jump;
+    public float jumpForce = 2.0f;
+    Rigidbody rb;
+
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
+        Jump = new Vector3(0.0f, 2.0f, 0.0f);
         thisAnimation = GetComponent<Animation>();
         thisAnimation["Flap_Legacy"].speed = 3;
     }
@@ -15,6 +21,10 @@ public class Player : MonoBehaviour
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Jump * jumpForce, ForceMode.Impulse);
             thisAnimation.Play();
+        }
+            
     }
 }
